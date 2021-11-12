fcfd84b5257b188e8cfb5770ecae41f7d287a3de
Jenil jain <54360821+jenil13@users.noreply.github.com>
2021-11-12 02:03:49 -0500

    Update guess.py

--- guess.py
+++ guess.py
@@ -1,35 +1,37 @@
+
+
 import random
 
-top_of_range = input("Type a number: ")
+taking_input_from_user = input("Type a number: ")
 
-if top_of_range.isdigit():
-    top_of_range = int(top_of_range)
+if taking_input_from_user.isdigit():
+    taking_input_from_user = int(taking_input_from_user)
 
-    if top_of_range <= 0:
-        print('Please type a number larger than 0 next time.')
+    if taking_input_from_user <= 0:
+        print('Invalid as the value entered less than zero')
         quit()
 else:
-    print('Please type a number next time.')
+    print("next try enter nu,mber greater than zero")
     quit()
 
-random_number = random.randint(0, top_of_range)
-guesses = 0
+random_number = random.randint(0, taking_input_from_user)
+counter_for_guesses = 0
 
 while True:
-    guesses += 1
-    user_guess = input("Make a guess: ")
-    if user_guess.isdigit():
-        user_guess = int(user_guess)
+    counter_for_guesses += 1
+    asking_user_guesses = input("Make a guess: ")
+    if asking_user_guesses.isdigit():
+        asking_user_guesses = int(asking_user_guesses)
     else:
         print('Please type a number next time.')
         continue
 
-    if user_guess == random_number:
+    if asking_user_guesses == random_number:
         print("You got it!")
         break
-    elif user_guess > random_number:
+    elif asking_user_guesses > random_number:
         print("You were above the number!")
     else:
         print("You were below the number!")
 
-print("You got it in", guesses, "guesses")
+print("You got it in", counter_for_guesses, "counter_for_guesses")
